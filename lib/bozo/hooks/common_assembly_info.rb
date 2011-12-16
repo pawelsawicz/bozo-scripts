e888228ff30ae7068e71bf4b61f0dcff72f34375
Garry Shutler <garryshutler@zopa.com>
2011-12-16 15:12:35 +0000

    Generating a common assembly info file

--- lib/bozo/hooks/common_assembly_info.rb
+++ lib/bozo/hooks/common_assembly_info.rb
@@ -6,10 +6,20 @@ module Bozo::Hooks
       Bozo.log_info 'Generating common assembly info'
       
       version = Bozo::Configuration.version
-      trademark = Bozo::ENV['GIT_HASH_FULL']
-
-      Bozo.log_debug version
-      Bozo.log_debug trademark
+      git_hash = Bozo::ENV['GIT_HASH_FULL']
+      path = File.expand_path(File.join('build', 'CommonAssemblyInfo.cs'))
+      
+      Bozo.log_debug "Version: #{version}"
+      Bozo.log_debug "Commit hash: #{git_hash}"
+      Bozo.log_debug "Path: #{path}"
+      
+      File.open(path, 'w+') do |f|
+        f << "using System.Reflection;"
+        f << "[assembly: AssemblyCompany(\"Zopa\")]"
+        f << "[assembly: AssemblyVersion(\"#{version}\")]"
+        f << "[assembly: AssemblyFileVersion(\"#{version}\")]"
+        f << "[assembly: AssemblyTrademark(\"#{git_hash}\")]"
+      end
     end
     
   end
