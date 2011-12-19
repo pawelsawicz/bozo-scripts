fb42f553a1d5094feaaa7f8b14f5c63a6a7316f6
lukesmith <stuff@lukesmith.net>
2011-12-19 17:30:36 +0000

    Using symbols rather than strings'

--- lib/bozo/hooks/teamcity.rb
+++ lib/bozo/hooks/teamcity.rb
@@ -4,7 +4,7 @@ module Bozo::Hooks
 
     def pre_compile
       return unless Teamcity.hosted_in_teamcity?
-      log_pre_step 'compile'
+      log_pre_step :compile
       version = Bozo::Configuration.version
 
       puts "##teamcity[buildNumber '#{version}']"
@@ -16,7 +16,7 @@ module Bozo::Hooks
     def post_compile
       return unless Teamcity.hosted_in_teamcity?
       puts "##teamcity[compilationFinished compiler='Bozo']"
-      log_post_step 'compile'
+      log_post_step :compile
     end
 
     def post_test
@@ -26,7 +26,7 @@ module Bozo::Hooks
       report_path = File.expand_path(File.join(Dir.pwd, "/temp/nunit/#{t}-report.xml"))
       puts "##teamcity[importData type='#{t}' path='#{report_path}']" if File.exist? report_path
 
-      log_post_step 'test'
+      log_post_step :test
     end
 
     def method_missing(method, *args)
