11239ff8d639c4b0a8a8529fc0b4044605a6245e
lukesmith <stuff@lukesmith.net>
2011-12-22 18:20:11 +0000

    using nil? know its wrong way but want to see what gets returned

--- lib/bozo/test_runners/dotcover.rb
+++ lib/bozo/test_runners/dotcover.rb
@@ -10,7 +10,7 @@ module Bozo::TestRunners
   # produces a separate dotcover output
   class DotCover
     def self.default_path
-      if ENV['TEAMCITY_VERSION'] != nil
+      if ENV['TEAMCITY_VERSION'].nil?
         File.join(ENV['teamcity.dotCover.home'], 'dotcover.exe')
       else
         File.join(ENV['ProgramFiles(x86)'], 'JetBrains', 'dotCover', 'v1.2', 'Bin', 'dotcover.exe')
