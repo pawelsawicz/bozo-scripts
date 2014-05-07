cb322e14840e084cbfc983a15fa91f9364f22f06
Kieran.Lynam <kieran.lynam@zopa.com>
2014-05-07 18:04:21 +0100

    modified nuget publisher to simplify script. removed directory as parameter.

--- lib/bozo/publishers/nuget.rb
+++ lib/bozo/publishers/nuget.rb
@@ -5,14 +5,6 @@ module Bozo::Publishers
   # Publisher that pushes package to nuget
   class Nuget
 
-    def initialize
-      @package_directories = []
-    end
-
-    def package_directories(*args)
-      @package_directories << args
-    end
-
     def server(server)
       @server = server
     end
@@ -22,31 +14,24 @@ module Bozo::Publishers
     end
 
     def execute
-      raise Bozo::ConfigurationError.new 'You must specify at least one source file or directory' if @package_directories.empty?
       raise Bozo::ConfigurationError.new 'You must specify a nuget server address' if @server.empty?
       raise Bozo::ConfigurationError.new 'You must specify a nuget api key' if @api_key.empty?
 
-      source_directories do |source_files|
-        source_files.each do |source_file|
-          push source_file
-        end
+      Dir[File.join('dist', 'nuget', '**', '*')].each do |source_file|
+        push File.expand_path(source_file)
       end
     end
 
     private
 
-    def source_directories
-      @package_directories.each do |dir|
-        relative_source_dir = File.join(dir)
-        source_dir = File.expand_path relative_source_dir
-        source_dir_path = Pathname.new source_dir
-        yield Dir[File.join(source_dir, '**', '*')]
-      end
-    end
-
     def push(source_file)
-      log_debug "Publishing package \"#{source_file}\" to \"#{@server}\""
-      `nuget push #{source_file} #{@api_key} -s #{@server}`
+      args = []
+      args << File.expand_path(File.join('build', 'tools', 'nuget', 'NuGet.exe'))
+      args << "push"
+      args << "\"#{source_file}\""
+      args << "\"#{@api_key}\""
+      args << "-s #{@server}"
+      execute_command :nuget, args
     end
 
   end
