9e76efdb5f55f5e287bc2116e384ac9fdf5cffa8
Kieran.Lynam <kieran.lynam@zopa.com>
2014-06-19 18:39:03 +0100

    ADDED: versioning hooks for windows deployment to use build number on pre-release

---
+++ lib/bozo/hooks/nuget_version.rb
@@ -0,0 +1,22 @@
+module Bozo::Hooks
+
+  class NugetVersion
+
+  	def post_dependencies
+      env['GIT_HASH_FULL'] = `git log -1 --format="%H"`.strip
+      env['BUILD_VERSION'] = build_version
+  	end
+
+  	private
+
+  	def build_version
+  	  if pre_release?
+  	  	Bozo::Versioning::Version.new(env['BUILD_NUMBER'],0,0)
+  	  else
+  	  	version
+  	  end
+  	end
+  
+  end
+
+end
