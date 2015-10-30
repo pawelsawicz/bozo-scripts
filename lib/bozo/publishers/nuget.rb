fe67ae4eb48e5fabcdefa96e275d9efcf09d275e
David Silcott <d.silcott@gmail.com>
2015-10-30 18:08:11 +0000

    Add ability to selectively publish packages

--- lib/bozo/publishers/nuget.rb
+++ lib/bozo/publishers/nuget.rb
@@ -5,6 +5,10 @@ module Bozo::Publishers
   # Publisher that pushes package to nuget
   class Nuget
 
+    def initialize
+      @packages = []
+    end
+
     def server(server)
       @server = server
     end
@@ -13,12 +17,21 @@ module Bozo::Publishers
       @api_key = api_key
     end
 
+    def package(package)
+      @packages << package
+    end
+
     def execute
       raise Bozo::ConfigurationError.new 'You must specify a nuget server address' if @server.empty?
-      raise Bozo::ConfigurationError.new 'You must specify a nuget api key' if @api_key.empty?
 
-      Dir[File.join('dist', 'nuget', '**', '*')].each do |source_file|
-        push File.expand_path(source_file)
+      if @packages.empty?
+        Dir[File.join('dist', 'nuget', '**', '*')].each do |source_file|
+          push File.expand_path(source_file)
+        end
+      else
+        @packages.each do |package|
+          push File.expand_path(package)
+        end
       end
     end
 
@@ -29,11 +42,13 @@ module Bozo::Publishers
       args << File.expand_path(File.join('build', 'tools', 'nuget', 'NuGet.exe'))
       args << "push"
       args << "\"#{source_file}\""
-      args << "\"#{@api_key}\""
       args << "-s #{@server}"
+
+      if !@api_key.nil?
+        args << "\"#{@api_key}\""
+      end
       execute_command :nuget, args
     end
-
   end
 
 end
